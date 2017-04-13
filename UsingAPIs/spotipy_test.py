e3b03fc472ddc3e11521300ad223b26998df8c4c
martinchoffman <martin.c.hoffman@gmail.com>
2017-04-13 17:03:45 -0500

    GetAlbums

--- UsingAPIs/spotipy_test.py
+++ UsingAPIs/spotipy_test.py
@@ -1,23 +1,56 @@
-import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
-from spotipy.client import Spotify
-
-client_credentials_manager = SpotifyClientCredentials(client_id="771fca1e6b1649e48b00189968912e6a",
-                                                      client_secret="4343723038b44a27aba4372d48e8fa7d")
-sp = Spotify(client_credentials_manager=client_credentials_manager)
-
-#chris = sp.search(q='ctordi7545', type='user')
-#print(chris)
-sp.user_playlist_create(user="ctordi7545", name="Leet Haxors", public=True)
-
-playlists = sp.user_playlists("ctordi7545")
-while playlists:
-    for i, playlist in enumerate(playlists['items']):
-        print("%4d %s %s" % (i + 1 + playlists['offset'], playlist['uri'],  playlist['name']))
-    if playlists['next']:
-        playlists = sp.next(playlists)
-    else:
-        playlists = None
-
 # Client ID:        771fca1e6b1649e48b00189968912e6a
 # Client Secret:    4343723038b44a27aba4372d48e8fa7d
+
+import spotipy
+import sys
+
+def main():
+    spotify = spotipy.Spotify()
+    function = sys.argv[1]
+
+    results = spotify.search(q='album: Graduation', type='album')
+    print(results)
+    #uri = results['artists']['items'][0]['uri']
+    #print(uri)
+
+    if function == 'getAlbums':
+        print('argv[2] ' + sys.argv[2])
+        artist = sys.argv[2]
+        getArtistURI(spotify, artist)
+    elif function == 'getTracks':
+        album = sys.argv[2]
+        #getAlbumURI(spotify, album)
+    else:
+        print('Usage: getAlbums ARTIST_NAME or getTracks ALBUM_NAME')
+
+def getArtistURI(spotify, artist):
+    results = spotify.search(q='artist: ' + artist, type='artist')
+    print(results)
+    uri = results['artists']['items']['external_urls']['uri']
+
+    results = spotify.artist_albums(uri, album_type='album')
+    albums = results['items']
+    while results['next']:
+        results = spotify.next(results)
+        albums.extend(results['items'])
+
+    for album in albums:
+        print(album['name'])
+
+'''def getAlbumURI(spotify, album):
+    album = ' ' + album
+
+    results = spotify.search(q='album:' + album, type='album')
+    uri = results['artists']['items'][0]['uri']
+
+    results = spotify.artist_albums(uri, album_type='album')
+    albums = results['items']
+    while results['next']:
+        results = spotify.next(results)
+        albums.extend(results['items'])
+
+    for album in albums:
+        print(album['name'])'''
+
+if __name__ == '__main__':
+    main()

741ed456f15821c8aece5ea5c682db2d179c672b
Alex Tordi <alextordi@eduroam-171-54.dyn.carleton.edu>
2017-04-24 22:40:00 -0500

    updatedurllib

--- WebApp/tests/api_tests.py
+++ WebApp/tests/api_tests.py
@@ -5,6 +5,8 @@
 '''
 
 import unittest
+import json
+import urllib.request
 
 class QueryTester(unittest.TestCase):
 	def setUP(self):
@@ -26,7 +28,10 @@ class QueryTester(unittest.TestCase):
 		queryValues: list of station ids we would find in state
 		knwonStationId: known station id
 	'''
-	def testStation(self):
+	def testStation(self): 
+		url = 'http://localhost:5000/stations/<state>/stn_id'
+		json_string = url.request.open(url).read()
+		result = json.loads(json_string)
 		queryValues = ["1234567", "1245643", "7654321"]
 		knownStationId = '1234567'
 		self.assertIn(knownStationId, queryValues)
@@ -36,6 +41,9 @@ class QueryTester(unittest.TestCase):
 		maxInTestData: Known max temp for south dakota in 2015
 	'''
 	def testMax(self):
+		url = 'http://localhost:5000/weather/max/<stn_ids>'
+		json_string = url.request.open(url).read()
+		result = json.loads(json_string)
 		queryValues = [5,16.9,19.8,20.8,26.2,30.7,31.8,32.5,38.5,38.7,39.7,42.3,42.3,44.8,52.9,72.5]
 		maxInTestData = 72.5
 		self.assertIn(maxInTestData,queryValues)
@@ -45,6 +53,9 @@ class QueryTester(unittest.TestCase):
 		maxInTestData: Known min temp for south dakota in 2015
 	'''
 	def testMin(self):
+		url = 'http://localhost:5000/weather/min/<stn_ids>'
+		json_string = url.request.open(url).read()
+		result = json.loads(json_string)
 		queryValues = [5,16.9,19.8,20.8,26.2,30.7,31.8,32.5,38.5,38.7,39.7,42.3,42.3,44.8,52.9,72.5]
 		minInTestData = 5
 		self.assertIn(minInTestData,queryValues)
@@ -54,6 +65,9 @@ class QueryTester(unittest.TestCase):
 		maxInTestData: Known mean temp for south dakota in 2015
 	'''
 	def testMean(self):
+		url = 'http://localhost:5000/weather/average/<stn_ids>'
+		json_string = url.request.open(url).read()
+		result = json.loads(json_string)
 		queryValues = [5,16.9,19.8,20.8,26.2,30.7,31.8,32.5,38.5,38.7,39.7,42.3,42.3,44.8,52.9,72.5]
 		meanInTestData = 34.7
 		self.assertEquals(meanInTestData,34.7)
@@ -63,6 +77,9 @@ class QueryTester(unittest.TestCase):
 		rainyDays: Known number of rainy days for South Dakota in 2015
 	'''
 	def testRainy(self):
+		url = 'http://localhost:5000/weather/rain_drizzle/<stn_ids>'
+		json_string = url.request.open(url).read()
+		result = json.loads(json_string)
 		queryValues = [[2, 3], [7, 26], [5, 29]]
 		rainyDays = 3
 		self.assertEquals(queryValues.length, rainyDays)
@@ -72,6 +89,9 @@ class QueryTester(unittest.TestCase):
 		rainyDays: Known number of snowy days for South Dakota in 2015
 	'''
 	def testSnowy(self):
+		url = 'http://localhost:5000/weather/snow_ice_pellets/<stn_ids>'
+		json_string = url.request.open(url).read()
+		result = json.loads(json_string)
 		queryValues = [[2, 3], [7, 26], [5, 29]]
 		snowyDays = 3
 		self.assertEquals(queryValues.length, snowyDays)

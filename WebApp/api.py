28c12cb14fc422a1d352547f2b2c9d1848718043
Alex Tordi <alextordi@eduroam-171-91.dyn.carleton.edu>
2017-05-08 09:22:04 -0500

    Added comments

--- WebApp/api.py
+++ WebApp/api.py
@@ -378,15 +378,6 @@ def get_city_info():
     
     return(city_list)
 
-@app.route('/range/city/days/<temp1>/<temp2>/<numCities>')
-def get_days(temp1, temp2, numCities):
-    '''
-        returns json dump with number of days where mean temp meets temp range.
-        List is in descending order and capped at 10 cities
-    ''' 
-    numCities = int(numCities)
-    return json.dumps(get_cities_days(temp1,temp2)[:numCities])
-
 
 @app.route('/stations/<state_name>')
 def get_num_stations(state_name):
@@ -471,6 +462,9 @@ def compare_cities(state_name1, city_name1, state_name2, city_name2):
 
 @app.route('/range/<temp1>/<temp2>')
 def find_states(temp1, temp2):
+    '''
+        returns json dump with information on mean temperature of states that fit user input range
+    '''
     temp1 = float(temp1)
     temp2 = float(temp2)
     state_mean_temp_list = [{"AL": 66.6}, {"AK": 37.8}, {"AZ": 66.5}, {"AR": 62.7}, {"CA": 61.7}, {"CO": 47.5}, {"CT": 53.2}, {"DC": 59.7}, {"DE": 57.5}, {"FL": 72.8}, {"GA": 66.2}, {"HI": 75.1}, {"ID": 47.2}, {"IL": 54.7}, {"IN": 54.0}, {"IA": 51.3}, {"KS": 57.7}, {"KY": 58.0}, {"LA": 70.5}, {"ME": 46.2}, {"MD": 58.0}, {"MA": 52.1}, {"MI": 48.3}, {"MN": 45.9}, {"MS": 66.5}, {"MO": 58.7}, {"MT": 46.0}, {"NE": 51.9}, {"NV": 58.4}, {"NH": 47.2}, {"NJ": 56.6}, {"NM": 57.3}, {"NY": 51.6}, {"NC": 62.0}, {"ND": 43.6}, {"OH": 53.6}, {"OK": 62.3}, {"OR": 52.5}, {"PA": 53.3}, {"RI": 53.1}, {"SC": 64.7}, {"SD": 50.0}, {"TN": 61.4}, {"TX": 69.1}, {"UT": 52.4}, {"VT": 46.6}, {"VA": 58.9}, {"WA": 52.4}, {"WV": 54.7}, {"WI": 47.4}, {"WY": 45.1}]
@@ -485,12 +479,23 @@ def find_states(temp1, temp2):
 
 @app.route('/range/city/<temp1>/<temp2>')
 def find_cities(temp1, temp2):
+    '''
+        returns json dump with cities whose mean temperature for 2016 fits range set by user
+    '''
     temp1 = float(temp1)
     temp2 = float(temp2)
     city_mean_temp_list = [{"NEW YORK": 52.8}, {"LOS ANGELES": 64.2}, {"CHICAGO": 53.7}, {"HOUSTON": 70.7}, {"PHOENIX": 76.4}, {"PHILADELPHIA": 58.3}, {"SAN ANTONIO": 70.5}, {"SAN DIEGO": 65.6}, {"DALLAS": 68.8}, {"SAN JOSE": 73.8}, {"DETROIT": 50.1}, {"JACKSONVILLE": 65.5}, {"INDIANAPOLIS": 53.9}, {"SAN FRANCISCO": 57.8}, {"COLUMBUS": 59.8}, {"AUSTIN": 60.9}, {"MEMPHIS": 64.4}, {"FORT WORTH": 67.7}, {"BALTIMORE": 58.5}, {"CHARLOTTE": 61.6}, {"EL PASO": 67.7}, {"BOSTON": 53.7}, {"SEATTLE": 54.3}, {"WASHINGTON": 53.2}, {"MILWAUKEE": 48.8}, {"DENVER": 52.2}, {"LOUISVILLE": 60.4}, {"LAS VEGAS": 61.0}, {"NASHVILLE": 62.6}, {"OKLAHOMA CITY": 62.4}, {"PORTLAND": 52.9}, {"TUCSON": 71.8}, {"ALBUQUERQUE": 59.0}, {"ATLANTA": 66.1}, {"LONG BEACH": 65.3}, {"FRESNO": 67.2}, {"SACRAMENTO": 61.9}, {"MESA": 58.8}, {"KANSAS CITY": 57.3}, {"CLEVELAND": 56.8}, {"OMAHA": 43.3}, {"MIAMI": 76.9}, {"OAKLAND": 55.6}, {"TULSA": 63.9}, {"HONOLULU": 77.1}, {"MINNEAPOLIS": 49.8}, {"COLORADO SPRINGS": 52.2}, {"ARLINGTON": 61.2}, {"WICHITA": 62.5}, {"RALEIGH": 58.0}, {"ST. LOUIS": 59.0}, {"TAMPA": 67.5}, {"CINCINNATI": 56.6}, {"PITTSBURGH": 54.2}, {"AURORA": 53.3}, {"TOLEDO": 52.8}, {"RIVERSIDE": 65.8}, {"STOCKTON": 65.6}, {"CORPUS CHRISTI": 74.4}, {"NEWARK": 55.8}, {"ANCHORAGE": 41.3}, {"BUFFALO": 49.1}, {"FORT WAYNE": 52.7}, {"GLENDALE": 72.3}, {"LINCOLN": 57.2}, {"HENDERSON": 63.2}, {"CHANDLER": 62.8}, {"SCOTTSDALE": 74.9}, {"BATON ROUGE": 69.3}, {"BIRMINGHAM": 65.6}, {"NORFOLK": 60.3}, {"MADISON": 51.5}, {"NEW ORLEANS": 71.1}, {"CHESAPEAKE": 58.6}, {"ORLANDO": 73.0}, {"LAREDO": 75.1}, {"LUBBOCK": 62.4}, {"RENO": 56.0}, {"AKRON": 53.8}, {"DURHAM": 62.0}, {"ROCHESTER": 51.7}, {"MONTGOMERY": 62.7}, {"FREMONT": 51.2}, {"SHREVEPORT": 68.4}, {"ARLINGTON": 61.2}, {"GLENDALE": 72.3}]
    
     return json.dumps(get_cities(temp1,temp2))
 
+@app.route('/range/city/days/<temp1>/<temp2>/<numCities>')
+def get_days(temp1, temp2, numCities):
+    '''
+        returns json dump with number of days where mean temp meets temp range.
+        List is in descending order and capped at 10 cities
+    ''' 
+    numCities = int(numCities)
+    return json.dumps(get_cities_days(temp1,temp2)[:numCities])
 
 
 

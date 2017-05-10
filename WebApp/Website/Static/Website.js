0f25c59f4b879f1fe89db41d18ce82c836fc8d14
Alex Tordi <alextordi@eduroam-171-91.dyn.carleton.edu>
2017-05-10 18:38:58 -0500

    Added compare feature

--- WebApp/Website/Static/Website.js
+++ WebApp/Website/Static/Website.js
@@ -26,6 +26,7 @@ function onGetCityButton() {
 
 
 function onGetStateButton() {
+
 	var state = document.getElementById("stateSearch").value;
 	var url = api_base_url + state;
 	xmlHttpRequest = new XMLHttpRequest();
@@ -40,10 +41,38 @@ function onGetStateButton() {
 	xmlHttpRequest.send(null);
 }
 
-function getStateCityCallback(responseText) {
-	var statesList = JSON.parse(responseText);
+function onGetCompareButton() {
+	var state1 = document.getElementById("stateC1").value;
+	var city1  = document.getElementById("cityC1").value;
+	var state2 = document.getElementById("stateC2").value;
+	var city2  = document.getElementById("cityC2").value;
+
+	if (city1 == '' && city2 == '') {
+		var url = api_base_url + 'compare/' + state1 + '/' + state2;
+	} else {
+		var url = api_base_url + 'compare/' + state1 + '/' + city1 + '/' + state2 + '/' + city2;
+	}
+
+	xmlHttpRequest = new XMLHttpRequest();
+	xmlHttpRequest.open('get', url);
+
+    xmlHttpRequest.onreadystatechange = function() {
+        if (xmlHttpRequest.readyState == 4 && xmlHttpRequest.status == 200) { 
+            getCompareCallback(xmlHttpRequest.responseText);
+        } 
+	};
+
+	xmlHttpRequest.send(null);
+	
+
+}
+
+function getCompareCallback(responseText) {
+	var compareList = JSON.parse(responseText);
 	var tableBody = '';
-    tableBody += '<tr>';
+	tableBody += '<tr>';
+    tableBody += '<td>' + 'State' + '</td>';
+    tableBody += '<td>' + 'City' + '</td>';
     tableBody += '<td>' + 'Max Temp' + '</td>';
     tableBody += '<td>' + 'Min Temp' + '</td>';
     tableBody += '<td>' + 'Mean Temp' + '</td>';
@@ -52,6 +81,50 @@ function getStateCityCallback(responseText) {
     tableBody += '</tr>';
 
     tableBody += '<tr>';
+    tableBody += '<td>' + compareList[0]['State'] + '</td>';
+    tableBody += '<td>' + compareList[0]['City'] + '</td>';
+    tableBody += '<td>' + compareList[0]['Max Temp'] + '</td>';
+    tableBody += '<td>' + compareList[2]['Min Temp'] + '</td>';
+    tableBody += '<td>' + compareList[4]['Mean Temp'] + '</td>';
+    tableBody += '<td>' + compareList[6]['Rain Index'] + '</td>';
+    tableBody += '<td>' + compareList[8]['Snow Index'] + '</td>';
+    tableBody += '</tr>';
+
+    tableBody += '<tr>';
+    tableBody += '<td>' + compareList[1]['State'] + '</td>';
+    tableBody += '<td>' + compareList[1]['City'] + '</td>';
+    tableBody += '<td>' + compareList[1]['Max Temp'] + '</td>';
+    tableBody += '<td>' + compareList[3]['Min Temp'] + '</td>';
+    tableBody += '<td>' + compareList[5]['Mean Temp'] + '</td>';
+    tableBody += '<td>' + compareList[7]['Rain Index'] + '</td>';
+    tableBody += '<td>' + compareList[9]['Snow Index'] + '</td>';
+    tableBody += '</tr>';
+
+
+    var resultsTableElement = document.getElementById('results_table');
+	resultsTableElement.innerHTML = tableBody;
+
+
+
+
+}
+
+function getStateCityCallback(responseText) {
+	var statesList = JSON.parse(responseText);
+	var tableBody = '';
+    tableBody += '<tr>';
+    tableBody += '<td>' + 'State' + '</td>';
+    tableBody += '<td>' + 'City' + '</td>';
+    tableBody += '<td>' + 'Max Temp' + '</td>';
+    tableBody += '<td>' + 'Min Temp' + '</td>';
+    tableBody += '<td>' + 'Mean Temp' + '</td>';
+    tableBody += '<td>' + 'Rain Index' + '</td>';
+    tableBody += '<td>' + 'Snow Index' + '</td>';
+    tableBody += '</tr>';
+
+    tableBody += '<tr>';
+    tableBody += '<td>' + statesList[0]['State'] + '</td>';
+    tableBody += '<td>' + statesList[0]['City'] + '</td>';
     tableBody += '<td>' + statesList[0]['Max Temp'] + '</td>';
     tableBody += '<td>' + statesList[1]['Min Temp'] + '</td>';
     tableBody += '<td>' + statesList[2]['Mean Temp'] + '</td>';
@@ -120,7 +193,17 @@ function onCompareNav() {
 						'	<li style="float: right"><a id="about" onclick="onAboutNav()">About</a></li>\n';
 
 	var page = document.getElementById('page');
-	page.innerHTML = '<h1>Compare</h1>\n';
+	page.innerHTML = '      <h1>Compare</h1>\n' +
+					 '<input type="search" id="stateC1" placeholder="Enter a state..">\n' +
+					 '<input type="search" id="cityC1" placeholder="Enter a city..">\n' +
+					 '<p style="display:inline"> vs. </p>\n' + 
+					 '<input type="search" id="stateC2" placeholder="Enter a state..">\n' +
+					 '<input type="search" id="cityC2" placeholder="Enter a city..">\n' +
+					 '<button id="authors_button" onclick="onGetCompareButton(\'Min Temp\')">Compare</button>\n' + 
+					 '<div id="assignment_content">\n' +
+					 '	<p><table id="results_table"> </table></p>\n' +
+					 '</div>';
+
 }
 
 function onAboutNav() {

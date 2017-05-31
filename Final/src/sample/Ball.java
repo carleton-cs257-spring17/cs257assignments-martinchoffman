7ca89d2691adcb8af18503ff0b19b02ba6a11c6a
Alex Tordi <alextordi@eduroam-171-142.dyn.carleton.edu>
2017-05-30 22:44:37 -0500

    Pathway works

---
+++ Final/src/sample/Ball.java
@@ -0,0 +1,39 @@
+/**
+ * Ball.java
+ * Jeff Ondich, 10/29/14.
+ *
+ * A sample subclass of Sprite for CS257.
+ */
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.shape.Circle;
+
+public class Ball extends Circle {
+    @FXML private double velocityX;
+    @FXML private double velocityY;
+
+    public Ball() {
+    }
+
+    public void step() {
+        this.setCenterX(this.getCenterX() + this.velocityX);
+        this.setCenterY(this.getCenterY() + this.velocityY);
+    }
+
+    public double getVelocityX() {
+        return velocityX;
+    }
+
+    public void setVelocityX(double velocityX) {
+        this.velocityX = velocityX;
+    }
+
+    public double getVelocityY() {
+        return velocityY;
+    }
+
+    public void setVelocityY(double velocityY) {
+        this.velocityY = velocityY;
+    }
+}
--- Final/src/sample/Controller.java
+++ Final/src/sample/Controller.java
@@ -30,6 +30,7 @@ public class Controller implements EventHandler<KeyEvent> {
     @FXML private AnchorPane gameBoard;
     @FXML private Button menuButton;
     @FXML private Button waveButton;
+    @FXML private Ball ball;
 
 
     private int score;
@@ -62,7 +63,41 @@ public class Controller implements EventHandler<KeyEvent> {
     }
 
     private void updateAnimation() {
+        double ballCenterX = this.ball.getCenterX() + this.ball.getLayoutX();
+        double ballCenterY = this.ball.getCenterY() + this.ball.getLayoutY();
+        double ballRadius = this.ball.getRadius();
+
+
+        // Bounce off walls
+        double ballVelocityX = this.ball.getVelocityX();
+        double ballVelocityY = this.ball.getVelocityY();
+
+        if (ballCenterX + ballRadius >= this.gameBoard.getWidth() - 140  && ballVelocityX > 0) {
+            this.ball.setVelocityX(0);
+            this.ball.setVelocityY(3);
+
+
+        } else if (ballCenterX - ballRadius < 50 && ballVelocityX < 0) {
+            this.ball.setVelocityX(0);
+            this.ball.setVelocityY(3);
+
+
+        } else if (ballCenterY + ballRadius >= 350 && ballCenterX + ballRadius > 150 && ballVelocityY > 0) {
+            this.ball.setVelocityY(0);
+            this.ball.setVelocityX(-3);
+
+        } else if (ballCenterY - ballRadius >= 600 && ballVelocityY > 0) {
+            this.ball.setVelocityY(0);
+            this.ball.setVelocityX(3);
+
+        } else if (ballCenterX + ballRadius >= this.gameBoard.getWidth() - 140 && ballCenterY - ballRadius >= 600) {
+            return;
+        }
+
+        // Move the sprite.
+        this.ball.step();
     }
+
 
     @Override
     public void handle(KeyEvent keyEvent) {
